206c7085b02910da87dee08318e39517fefaa17e
Alexandre Macabies <81353+zopieux@users.noreply.github.com>
2022-04-03 18:50:57 +0200

    Fix update and download URL
    Otherwise the script updates to the NL version of this... :facepalm:

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -9,8 +9,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
-// @updateURL    https://github.com/PlaceNL/Bot/raw/master/placenlbot.user.js
-// @downloadURL  https://github.com/PlaceNL/Bot/raw/master/placenlbot.user.js
+// @updateURL    https://raw.githubusercontent.com/Skeeww/Bot/master/placenlbot.user.js
+// @downloadURL  https://raw.githubusercontent.com/Skeeww/Bot/master/placenlbot.user.js
 // @grant        GM_getResourceText
 // @grant        GM_addStyle
 // ==/UserScript==
